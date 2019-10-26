7ac180db4be8b4d3729a679cd06f4b78c78c738d
bern <vlad6087@gmail.com>
2019-10-26 16:30:00 +0300

    bug fix

--- lab2/script4.sh
+++ lab2/script4.sh
@@ -3,4 +3,4 @@
 for file in $(grep -r -l '^#!/bin/.*' /bin)
 do
     grep -Eos "^#!/bin/[[:alnum:]]*" "$file" | sed "s|^|$file |"
-done | sort -k 2
+done | sort -k2 | uniq -c -f1 | sort -k1 -r -n | sed "1q" | awk '{print $2 " " $3}'
